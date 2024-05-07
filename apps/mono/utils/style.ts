729f6f898a226bb94a31b0edcc82a1dfa1611c12
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-05-07 09:40:07 +0700

    style helper boilerplate

---
+++ apps/mono/utils/style.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+export const iStyles = StyleSheet.create({
+	contentContainer: {
+		maxWidth: 1280,
+		marginHorizontal: 'auto',
+		padding: 18,
+	},
+});
