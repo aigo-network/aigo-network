84e5dd959359f8c10f776935c45a224f7f3d45e2
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-05-09 09:07:17 +0700

    module alias config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+	presets: ['module:@react-native/babel-preset'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				alias: {
+					assets: './src/assets',
+					components: './src/components',
+					utils: './src/utils',
+					state: './src/state',
+					stacks: './src/stacks',
+					screens: './src/screens',
+				},
+			},
+		]
+	],
 };
