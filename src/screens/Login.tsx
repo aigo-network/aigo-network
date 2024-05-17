4885922a98711b36b4fd4ed006d34ba1cc34df07
Tan Le <tanlethanh8266@gmail.com>
2024-05-17 10:51:56 +0700

    chore: add app icon svg for high resolution

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,104 +1,115 @@
-import { Image, StyleSheet, Text, View } from "react-native";
-import SafeContainer from "components/SafeContainer";
-import { handleSignInGoogle, handleSignInApple } from "utils/auth";
-import { Button } from "components/Button";
-import { FirebaseAuthTypes } from "@react-native-firebase/auth";
-import { appActions } from "state/app";
+import { Image, StyleSheet, Text, View } from 'react-native';
+import type { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import AppIcon from 'components/AppIcon';
+import { Button } from 'components/Button';
+import SafeContainer from 'components/SafeContainer';
+import { appActions } from 'state/app';
+import { handleSignInApple, handleSignInGoogle } from 'utils/auth';
 
 export const LoginScreen = () => {
-	const handleSignIn = async (callback: () => Promise<FirebaseAuthTypes.User>) => {
+	const handleSignIn = async (
+		callback: () => Promise<FirebaseAuthTypes.User>,
+	) => {
 		const user = await callback();
 		appActions.setUser(user);
-	}
+	};
 
-    return (
-        <View style={styles.container}>
-			<Image source={require('assets/img/login/background-logo.png')} style={styles.backgroundImage} />
-            <SafeContainer style={styles.safeContainer}>
+	return (
+		<View style={styles.container}>
+			<Image
+				source={require('assets/img/login/background-logo.png')}
+				style={styles.backgroundImage}
+			/>
+			<SafeContainer style={styles.safeContainer}>
 				<View>
-					<Image source={require('assets/img/login/logo.png')} style={styles.logoImg} />
+					<View style={styles.logoImg}>
+						<AppIcon />
+					</View>
 					<Text style={styles.welcome}>{`Welcome to\nAiGO`}</Text>
-					<Text style={styles.slogan}>{'Turns your everyday\nActions into rewards'}</Text>
+					<Text style={styles.slogan}>
+						{'Turns your everyday\nActions into rewards'}
+					</Text>
 				</View>
-                <View style={styles.btnGroup}>
-                    <Button prefix={
-                        	<Image source={require('assets/img/login/google-logo.png')} />
-                    	}
+				<View style={styles.btnGroup}>
+					<Button
+						prefix={
+							<Image source={require('assets/img/login/google-logo.png')} />
+						}
 						style={styles.btn}
 						onPress={() => handleSignIn(handleSignInGoogle)}
 					>
-                        <Text style={styles.btnText}>Log in with Google</Text>
-                    </Button>
-                    <Button prefix={
-                        	<Image source={require('assets/img/login/apple-logo.png')} />
-                    	}
+						<Text style={styles.btnText}>Log in with Google</Text>
+					</Button>
+					<Button
+						prefix={
+							<Image source={require('assets/img/login/apple-logo.png')} />
+						}
 						style={styles.btn}
 						onPress={() => handleSignIn(handleSignInApple)}
 					>
-                        <Text style={styles.btnText}>Log in with Apple</Text>
-                    </Button>
-                </View>
+						<Text style={styles.btnText}>Log in with Apple</Text>
+					</Button>
+				</View>
 				<Text style={styles.version}>Version 0.1</Text>
-            </SafeContainer>
-        </View>
-    )
-}
+			</SafeContainer>
+		</View>
+	);
+};
 
 export default LoginScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#6740ff',
-    },
+	container: {
+		flex: 1,
+		backgroundColor: '#6740ff',
+	},
 	backgroundImage: {
 		position: 'absolute',
 		bottom: 0,
 		left: 200,
 	},
-    safeContainer: {
-        justifyContent: 'space-between',
-    },
-    logoImg: {
+	safeContainer: {
+		justifyContent: 'space-between',
+	},
+	logoImg: {
 		alignSelf: 'center',
-        aspectRatio: 150 / 138,
 		marginTop: 120,
-        marginBottom: 24,
-    },
-    welcome: {
-        fontFamily: 'Gabarito',
-        fontWeight: '600',
-        fontSize: 42,
-        textAlign: 'center',
-        color: '#fff',
-    },
-    slogan: {
-        alignSelf: 'center',
-        marginTop: 14,
-        fontFamily: 'Gabarito',
-        fontSize: 19,
-        color: '#a0fa82',
-    },
-    btnGroup: {
+		marginBottom: 24,
+	},
+	welcome: {
+		fontFamily: 'Gabarito',
+		fontWeight: '600',
+		fontSize: 42,
+		textAlign: 'center',
+		color: '#fff',
+	},
+	slogan: {
+		alignSelf: 'center',
+		marginTop: 14,
+		fontFamily: 'Gabarito',
+		fontSize: 19,
+		color: '#a0fa82',
+	},
+	btnGroup: {
 		flex: 1,
-        gap: 17,
-        paddingHorizontal: 40,
+		gap: 17,
+		paddingHorizontal: 40,
 		justifyContent: 'flex-end',
 		paddingBottom: 50,
-    },
-	btn:{
+	},
+	btn: {
 		paddingVertical: 15,
 		backgroundColor: '#ededed',
 		borderRadius: 50,
 	},
-    btnText: {
-        textAlign: 'center',
+	btnText: {
+		textAlign: 'center',
 		fontFamily: 'Gabarito',
 		fontSize: 20,
-    },
+	},
 	version: {
 		fontFamily: 'Gabarito',
 		textAlign: 'center',
 		color: '#fff',
-	}
-})
+	},
+});
