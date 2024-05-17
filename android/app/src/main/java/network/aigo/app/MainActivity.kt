c3102e7cd25db5b89ff0d2931bdbedeaf9ca56a2
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-05-18 05:42:18 +0700

    chore: name space for Android app as network.aigo.app

--- android/app/src/main/java/network/aigo/app/MainActivity.kt
+++ android/app/src/main/java/network/aigo/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.aigo
+package network.aigo.app
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/network/aigo/app/MainApplication.kt
+++ android/app/src/main/java/network/aigo/app/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.aigo
+package network.aigo.app
 
 import android.app.Application
 import com.facebook.react.PackageList
