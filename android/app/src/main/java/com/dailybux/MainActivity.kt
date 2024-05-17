10fbceeb9590675e15ca0bd32ed9443e3b621ca9
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-05-18 02:27:03 +0700

    chore: rename the App to AiGO

--- android/app/src/main/java/com/dailybux/MainActivity.kt
+++ android/app/src/main/java/com/dailybux/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.dailybux
+package com.aigo
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "dailybux"
+  override fun getMainComponentName(): String = "aigo"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/dailybux/MainApplication.kt
+++ android/app/src/main/java/com/dailybux/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.dailybux
+package com.aigo
 
 import android.app.Application
 import com.facebook.react.PackageList
