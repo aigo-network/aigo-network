6b2dbe0f22a5e179f4f0747f5f23ae973326b1b0
Tan Le <tanlethanh8266@gmail.com>
2024-05-17 16:16:24 +0700

    chore: use config from utils

--- src/utils/auth/signinGoogle.ts
+++ src/utils/auth/signinGoogle.ts
@@ -1,9 +1,9 @@
-import Config from 'react-native-config';
 import auth from '@react-native-firebase/auth';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import { config } from 'utils/config';
 
 GoogleSignin.configure({
-	webClientId: Config.GOOGLE_SIGNIN_CLIENT_ID,
+	webClientId: config.GOOGLE_SIGNIN_CLIENT_ID,
 });
 
 export const handleSignInGoogle = async () => {
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,12 @@
+import Config from 'react-native-config';
+
 type AppConfig = {
-	name: string;
 	version: string;
+	GRAPHQL_API_ENDPOINT: string;
+	GOOGLE_SIGNIN_CLIENT_ID: string;
 };
 
-export const config: AppConfig = require('../../package.json');
+export const config: AppConfig = {
+	...require('../../package.json'),
+	...Config,
+};
