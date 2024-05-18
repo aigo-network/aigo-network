80c1df18f74670d6060c499c87d99bdde2f4c55f
Tan Le <tanlethanh8266@gmail.com>
2024-05-18 20:05:51 +0700

    chore: add social link in profile screen

--- src/screens/Profile/Footer.tsx
+++ src/screens/Profile/Footer.tsx
@@ -1,4 +1,10 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+	Linking,
+	StyleSheet,
+	Text,
+	TouchableOpacity,
+	View,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import LogOut from 'components/icon/LogOut';
 import Telegram from 'components/icon/Telegram';
@@ -7,6 +13,15 @@ import { logOut } from 'utils/auth';
 
 export const Footer = () => {
 	const { reset } = useNavigation();
+
+	const handlePressFollowTwitter = () => {
+		Linking.openURL('https://x.com/AIGO_Network');
+	};
+
+	const handlePressJoinTelegram = () => {
+		Linking.openURL('https://t.me/aigo_network');
+	};
+
 	const handleLogOut = () => {
 		reset({
 			index: 1,
@@ -20,10 +35,18 @@ export const Footer = () => {
 			<View style={styles.aboutContainer}>
 				<Text style={styles.title}>About AiGO Network</Text>
 				<View style={styles.socialContainer}>
-					<TouchableOpacity style={styles.socialButton}>
+					<TouchableOpacity
+						style={styles.socialButton}
+						onPress={handlePressFollowTwitter}
+						hitSlop={1}
+					>
 						<Twitter width={20} />
 					</TouchableOpacity>
-					<TouchableOpacity style={styles.socialButton}>
+					<TouchableOpacity
+						style={styles.socialButton}
+						onPress={handlePressJoinTelegram}
+						hitSlop={1}
+					>
 						<Telegram width={20} />
 					</TouchableOpacity>
 				</View>
@@ -58,7 +81,7 @@ const styles = StyleSheet.create({
 	},
 	socialContainer: {
 		flexDirection: 'row',
-		gap: 8,
+		gap: 10,
 	},
 	socialButton: {
 		width: 40,
