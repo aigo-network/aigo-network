86c35d584cfe122c826c8c14f72dc8d8b05158c7
Tan Le <tanlethanh8266@gmail.com>
2024-05-18 16:02:14 +0700

    feat: point popup after onboarding

---
+++ src/components/PointPopup.tsx
@@ -0,0 +1,72 @@
+import type { FC } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import X from './icon/X';
+
+type Props = {
+	point: number;
+	description: string;
+	onPressClose?: () => void;
+};
+
+export const PointPopup: FC<Props> = ({ point, description }) => {
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity style={styles.closeButton} hitSlop={14}>
+				<X color={'#9F9F9F'} width={14} />
+			</TouchableOpacity>
+			<Image
+				style={styles.pointTick}
+				source={require('assets/img/point-tick.png')}
+			/>
+			<View>
+				<Text style={styles.titleText}>
+					{'Yay! You’ve got '}
+					<Text style={styles.highlightText}>{`${point} GO`}</Text>
+				</Text>
+			</View>
+			<Text style={styles.descriptionText}>{description}</Text>
+		</View>
+	);
+};
+
+export default PointPopup;
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#fff',
+		maxWidth: 340,
+		gap: 20,
+		alignItems: 'center',
+		padding: 30,
+		paddingBottom: 40,
+		paddingTop: 60,
+		borderRadius: 20,
+	},
+	closeButton: {
+		backgroundColor: '#F0F0F0',
+		padding: 10,
+		borderRadius: 20,
+		position: 'absolute',
+		right: 10,
+		top: 10,
+	},
+	pointTick: {
+		width: 130,
+		height: 130,
+	},
+	descriptionText: {
+		fontSize: 16,
+		color: '#898989',
+		textAlign: 'center',
+	},
+	titleText: {
+		fontSize: 22,
+		color: '#000000',
+	},
+	highlightText: {
+		fontSize: 22,
+		color: '#6740FF',
+		fontWeight: '600',
+	},
+});
--- src/screens/Onboard/shared.tsx
+++ src/screens/Onboard/shared.tsx
@@ -1,5 +1,8 @@
+import Animated, { FadeInDown } from 'react-native-reanimated';
 import auth from '@react-native-firebase/auth';
 import { graphqlClient } from 'api/graphql';
+import { Align, showModal } from 'components/Modal';
+import PointPopup from 'components/PointPopup';
 import { appActions, appState } from 'state/app';
 import { defaultAvatar } from 'utils/misc';
 
@@ -27,4 +30,20 @@ export const completeOnboarding = async () => {
 
 	const { completeOnboarding: user } = await graphqlClient.completeOnboarding();
 	if (user) appActions.setAppUser(user);
+
+	setTimeout(() => {
+		showModal(
+			<Animated.View entering={FadeInDown}>
+				<PointPopup
+					point={user?.GOPoints || 50}
+					description="Your account is ready to use. You will be redirected to the Home Page in a few seconds."
+				/>
+			</Animated.View>,
+			{
+				id: 'onboarding-point-popup',
+				showBackdrop: true,
+				align: Align.CenterCenter,
+			},
+		);
+	}, 500);
 };
