7ab23a12a9557f277674afcf84c328a2dfde529e
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2024-05-21 18:10:03 +0700

    chore: Android display name in Home screen

--- src/utils/auth/signinApple.ts
+++ src/utils/auth/signinApple.ts
@@ -1,20 +1,48 @@
-import { appleAuth } from '@invertase/react-native-apple-authentication';
+import { Platform } from 'react-native';
+import uuid from 'react-native-uuid';
+import {
+	appleAuth,
+	appleAuthAndroid,
+} from '@invertase/react-native-apple-authentication';
 import auth from '@react-native-firebase/auth';
 import { setJWT } from 'api/jwt';
+import { config } from 'utils/config';
 
 import type { SignInFunction } from './types';
 
 export const signInWithApple: SignInFunction = async () => {
-	const appleAuthRequestResponse = await appleAuth.performRequest({
-		requestedOperation: appleAuth.Operation.LOGIN,
-		requestedScopes: [appleAuth.Scope.FULL_NAME],
-	});
+	let identityToken: string | undefined | null;
+	let nonce: string | undefined = uuid.v4() as string;
 
-	if (!appleAuthRequestResponse.identityToken) {
+	if (Platform.OS === 'ios') {
+		const authResponse = await appleAuth.performRequest({
+			requestedOperation: appleAuth.Operation.LOGIN,
+			requestedScopes: [appleAuth.Scope.FULL_NAME],
+		});
+
+		identityToken = authResponse.identityToken;
+		nonce = authResponse.nonce;
+	} else if (Platform.OS === 'android') {
+		console.log(config.FIREBASE_APPLE_AUTH_SERVICE_ID, '<-- serviceId');
+		console.log(config.FIREBASE_APPLE_AUTH_REDIRECT_URL, '<-- redirectUri');
+		appleAuthAndroid.configure({
+			clientId: config.FIREBASE_APPLE_AUTH_SERVICE_ID,
+			redirectUri: config.FIREBASE_APPLE_AUTH_REDIRECT_URL,
+			responseType: appleAuthAndroid.ResponseType.ALL,
+			scope: appleAuthAndroid.Scope.NAME,
+			nonce,
+			state: uuid.v4() as string,
+		});
+		const authResponse = await appleAuthAndroid.signIn();
+
+		identityToken = authResponse.id_token;
+		nonce = authResponse.nonce;
+	}
+
+	if (!identityToken) {
 		throw new Error('Apple Sign-In failed');
 	}
 
-	const { identityToken, nonce } = appleAuthRequestResponse;
 	const appleCredential = auth.AppleAuthProvider.credential(
 		identityToken,
 		nonce,
