8cb1ff1271ec6186e0a6fa6727efd2d478c1317f
Tan Le <tanlethanh8266@gmail.com>
2024-05-30 18:35:33 +0700

    chore: remove unused config variable

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,11 +1,8 @@
 import Config from 'react-native-config';
 
-import { name, version } from '../../package.json';
-
 import { activity } from './activity.json';
 
 type AppConfig = {
-	version: string;
 	FIREBASE_IOS_CLIENT_ID: string;
 	FIREBASE_IOS_REVERSED_CLIENT_ID: string;
 	FIREBASE_WEB_CLIENT_ID: string;
@@ -28,8 +25,6 @@ type ActivityConfig = {
 };
 
 export const config: AppConfig = {
-	name,
-	version,
 	...Config,
 	activity,
 } as never;
