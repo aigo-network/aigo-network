ddf4cc149b16da074ce0534baf1defe1d211646e
FME849 <1414439@gmail.com>
2024-06-17 16:39:53 +0700

    update: page banner

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,33 +1,19 @@
 'use client';
 import { useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
-import Animated, {
-	useAnimatedStyle,
-	useSharedValue,
-	withTiming,
-} from 'react-native-reanimated';
+import { StyleSheet, Text, View } from 'react-native';
+import { Defs, LinearGradient, Rect, Stop, Svg } from 'react-native-svg';
+import Image from 'next/image';
 
 import '../utils/auth';
 
-const AnimatedView = Animated.createAnimatedComponent(View);
+import leftGrid from './left-grid.svg';
+import rightGrid from './right-grid.svg';
+
+import BlurBackground from '@/components/BlurBackground';
+import Header from '@/components/Header';
 
 export default function Home() {
 	const [, setRender] = useState({});
-
-	const offset = useSharedValue(0);
-	const animatedStyle = useAnimatedStyle(() => {
-		return {
-			transform: [{ translateX: withTiming(offset.value) }],
-		};
-	}, [offset]);
-
-	useEffect(() => {
-		const interval = setInterval(() => {
-			offset.value = offset.value === 0 ? 200 : 0;
-		}, 2000);
-
-		return () => clearInterval(interval);
-	}, []);
 
 	useEffect(function updateState() {
 		//  This effect makes reanimated work
@@ -35,10 +21,87 @@ export default function Home() {
 	}, []);
 
 	return (
-		<AnimatedView style={[{ alignItems: 'center' }, animatedStyle]}>
-			<Text style={{ fontSize: 50, fontWeight: '900', color: '#fff' }}>
-				Hello World
-			</Text>
-		</AnimatedView>
+		<View style={styles.container}>
+			<View style={styles.banner}>
+				<Svg style={styles.bannerBackground} width="100%" height="auto">
+					<Defs>
+						<LinearGradient id="background-banner" x1={0} y1={0} x2={1} y2={0}>
+							<Stop offset={0} stopColor="#625bf6" stopOpacity={1} />
+							<Stop offset={1} stopColor="#81ddfb" stopOpacity={1} />
+						</LinearGradient>
+					</Defs>
+					<Rect width="100%" height="100%" fill="url(#background-banner)" />
+				</Svg>
+				<Image src={leftGrid} alt="left grid image" style={styles.leftGrid} />
+				<Image
+					src={rightGrid}
+					alt="right grid image"
+					style={styles.rightGrid}
+				/>
+				<Header style={styles.headerContainer} />
+				<View style={styles.bannerTitleContainer}>
+					<BlurBackground style={styles.subTitle}>
+						<Text style={styles.subTitleText}>
+							DECENTRALIZED MOBILITY DATA NETWORK
+						</Text>
+					</BlurBackground>
+					<Text style={styles.title}>
+						{'Neque Porro Quisquam\nClorem Siz Ipsum.'}
+					</Text>
+				</View>
+			</View>
+		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#04080b',
+	},
+	banner: {
+		position: 'relative',
+		gap: 20,
+	},
+	bannerBackground: {
+		position: 'absolute',
+	},
+	leftGrid: {
+		position: 'absolute',
+		height: '100%',
+		width: 'auto',
+	},
+	rightGrid: {
+		position: 'absolute',
+		height: '100%',
+		width: 'auto',
+		right: 0,
+	},
+	headerContainer: {
+		marginHorizontal: 48,
+		paddingVertical: 12,
+	},
+	bannerTitleContainer: {
+		gap: 16,
+	},
+	subTitle: {
+		alignSelf: 'center',
+		borderRadius: 30,
+		paddingHorizontal: 12,
+		paddingVertical: 8,
+	},
+	subTitleText: {
+		fontSize: 12,
+		fontWeight: '500',
+		lineHeight: 9,
+		letterSpacing: 2,
+		color: '#fff',
+	},
+	title: {
+		textAlign: 'center',
+		fontSize: 56,
+		fontWeight: '500',
+		lineHeight: 64,
+		color: '#d4e0ff',
+		paddingBottom: 75,
+	},
+});
