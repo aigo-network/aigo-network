1b7f2e79d2d889513302870b56ecd8798b486e5a
FME849 <1414439@gmail.com>
2024-06-17 16:39:08 +0700

    update: add BlurBackground component

---
+++ apps/web/src/components/BlurBackground.tsx
@@ -0,0 +1,28 @@
+import type { FC, ReactNode } from 'react';
+import type { ViewStyle } from 'react-native';
+import { ImageBackground, StyleSheet } from 'react-native';
+
+interface Props {
+	children: ReactNode;
+	style?: ViewStyle;
+}
+
+const BlurBackground: FC<Props> = ({ children, style }) => {
+	return (
+		<ImageBackground style={[styles.container, style]} blurRadius={128}>
+			{children}
+		</ImageBackground>
+	);
+};
+
+export default BlurBackground;
+
+const styles = StyleSheet.create({
+	container: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderWidth: 1,
+		borderColor: 'rgba(255, 255, 255, 0.2)',
+		backgroundColor: 'rgba(255, 255, 255, 0.16)',
+	},
+});
