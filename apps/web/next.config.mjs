13e33abf76be2fcd1aff8e1a81164de32f84f698
FME849 <1414439@gmail.com>
2024-06-17 10:23:03 +0700

    update: nextjs config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,40 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from 'path';
+import url from 'url';
+
+import webpack from 'webpack';
+const { DefinePlugin } = webpack;
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+console.log(path.resolve(__dirname, './raf.js'));
+const nextConfig = {
+	swcMinify: true,
+	transpilePackages: [
+		'react',
+		'react-dom',
+		'react-native',
+		'react-native-reanimated',
+	],
+	webpack: (config, { dev }) => {
+		config.resolve.alias = {
+			...(config.resolve.alias || {}),
+			'react-native$': 'react-native-web',
+		};
+
+		config.resolve.extensions = [
+			'.web.js',
+			'.web.ts',
+			'.web.tsx',
+			...config.resolve.extensions,
+		];
+
+		config.plugins.push(
+			new DefinePlugin({
+				__DEV__: dev,
+			}),
+		);
+
+		return config;
+	},
+};
 
 export default nextConfig;
