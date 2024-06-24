4a68b7eb9009c667443c564ffba7f8724effa0d8
Tan Le <tanlethanh8266@gmail.com>
2024-06-24 14:15:15 +0700

    chore: beauty scale bar

--- apps/mobile/src/screens/Map/index.tsx
+++ apps/mobile/src/screens/Map/index.tsx
@@ -1,10 +1,15 @@
-import { StyleSheet, View } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { MapView } from '@rnmapbox/maps';
 
 export const MapScreen = () => {
+	const insets = useSafeAreaInsets();
+	const top = Platform.OS === 'ios' ? 0 : Math.max(insets.top, 20);
+	const left = Math.max(insets.left, 20);
+
 	return (
 		<View style={styles.container}>
-			<MapView style={styles.map} />
+			<MapView style={styles.map} scaleBarPosition={{ top, left }}></MapView>
 		</View>
 	);
 };
