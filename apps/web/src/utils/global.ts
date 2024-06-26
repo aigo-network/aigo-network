2bad795784c07d9c4368c05faba8dc2486afb260
FME849 <1414439@gmail.com>
2024-06-26 09:23:00 +0700

    update: change default font style

--- apps/web/src/utils/global.ts
+++ apps/web/src/utils/global.ts
@@ -1,17 +1,17 @@
 import { Text, TextInput } from 'react-native';
 import { setDefaultProps } from '@aigo/config';
 
-import { interTight } from './style';
+import { poppins } from './style';
 
 setDefaultProps(Text, {
 	style: {
-		fontFamily: interTight.style.fontFamily,
+		fontFamily: poppins.style.fontFamily,
 		color: '#fff',
 	},
 });
 setDefaultProps(TextInput, {
 	style: {
-		fontFamily: interTight.style.fontFamily,
+		fontFamily: poppins.style.fontFamily,
 		color: '#fff',
 	},
 });
--- apps/web/src/utils/style.ts
+++ apps/web/src/utils/style.ts
@@ -1,3 +1,8 @@
-import { Inter_Tight } from 'next/font/google';
+import { Inter_Tight, Poppins, Righteous } from 'next/font/google';
 
 export const interTight = Inter_Tight({ subsets: ['latin'] });
+export const poppins = Poppins({
+	subsets: ['latin'],
+	weight: ['400', '500', '600', '700'],
+});
+export const righteous = Righteous({ subsets: ['latin'], weight: ['400'] });
