4c486d3bf647d9a55623195dbcc1893dcbdbbd13
FME849 <1414439@gmail.com>
2024-06-26 17:29:21 +0700

    update: add event param `appId`

--- apps/web/src/utils/analytic.ts
+++ apps/web/src/utils/analytic.ts
@@ -6,15 +6,15 @@ export const analytics = app ? getAnalytics(app) : null;
 
 export const tracker = {
 	logEvents: (name: string, params: Record<string, unknown>) => {
-		console.log('log event');
 		if (!analytics) return;
-		logEvent(analytics, name, params);
+		logEvent(analytics, name, { ...params, appId: 'quest' });
 	},
 	logScreenView: (firebase_screen: string, firebase_screen_class: string) => {
 		if (!analytics) return;
 		return logEvent(analytics, 'screen_view', {
 			firebase_screen,
 			firebase_screen_class,
+			appId: 'quest',
 		});
 	},
 };
