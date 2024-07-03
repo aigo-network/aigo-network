b7e94def61ca7be1b34239d696f2b27e222c880b
FME849 <1414439@gmail.com>
2024-07-03 15:13:30 +0700

    feat: impl responsive Header component

---
+++ apps/landing/src/app/section/Header/index.tsx
@@ -0,0 +1,96 @@
+import type { FC } from 'react';
+import styled from 'styled-components';
+
+import AiGOLogo from '@/components/icon/AiGOLogo';
+import AiGOText from '@/components/icon/AiGOText';
+import useDimension from '@/utils/hook/useDimension';
+
+const Header: FC = () => {
+	useDimension();
+
+	return (
+		<Container>
+			<LogoGroup>
+				<AiGOLogo size={50} />
+				<AiGOText size={95} />
+			</LogoGroup>
+			<NavBar>
+				<NavLink href="#">AiGO Ride App</NavLink>
+				<NavLink href="#">Docs</NavLink>
+			</NavBar>
+			<BurgerMenu>
+				<span></span>
+				<span></span>
+				<span></span>
+			</BurgerMenu>
+		</Container>
+	);
+};
+
+export default Header;
+
+const Container = styled.header`
+	position: fixed;
+	left: 0;
+	right: 0;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100%;
+	padding: 15px 25px;
+	margin: 0 auto;
+
+	@media (min-width: 576px) {
+		max-width: 540px;
+	}
+
+	@media (min-width: 768px) {
+		max-width: 720px;
+	}
+	@media (min-width: 992px) {
+		max-width: 960px;
+	}
+	@media (min-width: 1200px) {
+		max-width: 1140px;
+	}
+`;
+
+const LogoGroup = styled.a`
+	display: flex;
+	align-items: center;
+	gap: 8px;
+`;
+
+const NavBar = styled.nav`
+	display: none;
+
+	@media (min-width: 992px) {
+		display: flex;
+		gap: 20px;
+	}
+`;
+
+const NavLink = styled.a`
+	text-decoration: none;
+	padding: 5px;
+	color: var(--primary-color);
+`;
+
+const BurgerMenu = styled.div`
+	cursor: pointer;
+	align-self: center;
+
+	span {
+		border-radius: 3px;
+		display: block;
+		height: 3px;
+		margin-top: 5px;
+		transition-duration: 0.3s;
+		width: 30px;
+		background: var(--primary-color);
+	}
+
+	@media (min-width: 992px) {
+		display: none;
+	}
+`;
