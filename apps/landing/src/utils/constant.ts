9c7cb951e61cb7c088f60db474fed5b19e040652
FME849 <1414439@gmail.com>
2024-07-10 08:35:33 +0700

    feat: add website metadata

---
+++ apps/landing/src/utils/constant.ts
@@ -0,0 +1,9 @@
+export const TITLE = 'AiGO Network';
+export const DESCRIPTION = 'Decentralized Mobility Data Network';
+export const DOMAIN_URL = 'https://aigo.network';
+export const PROFILES = {
+	twitter: {
+		username: '@AIGO_Network',
+		link: 'https://twitter.com/AIGO_Network',
+	},
+};
