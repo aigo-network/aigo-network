661639974462530c5ae831635052f57c1604590b
FME849 <1414439@gmail.com>
2024-07-25 16:32:17 +0700

    feat: initialize color theme

--- apps/mobile/src/utils/global.ts
+++ apps/mobile/src/utils/global.ts
@@ -1,15 +1,33 @@
 import { Text, TextInput } from 'react-native';
 import { setDefaultProps } from '@aigo/config';
 
+export const defaultTheme = {
+	bgLight: '#ffffff',
+	bgGray: '#f4f5f6',
+	textLight: '#fdfdfd',
+	textDark100: '#000000',
+	textDark90: '#171717',
+	textDark80: '#464646',
+	textDark70: '#5e5e5e',
+	textDark50: '#8c8c8c',
+	textDark30: '#bbbbbb',
+	textDark20: '#d3d3d3',
+	cta100: '#34c3f4',
+	cta10: '#ebf9fe',
+	gray20: '#f1f2f3',
+	gray10: '#f8fafb',
+	red: '#d84a4a',
+};
+
 setDefaultProps(Text, {
 	style: {
-		fontFamily: 'Gabarito',
-		color: '#fff',
+		fontFamily: 'Lato',
+		color: defaultTheme.textLight,
 	},
 });
 setDefaultProps(TextInput, {
 	style: {
-		fontFamily: 'Gabarito',
-		color: '#fff',
+		fontFamily: 'Lato',
+		color: defaultTheme.textDark90,
 	},
 });
