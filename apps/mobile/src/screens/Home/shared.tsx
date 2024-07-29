bf245204acb907e955df8d0888c386183ad7aba4
FME849 <1414439@gmail.com>
2024-07-29 11:57:00 +0700

    chore: update invite code modal color

--- apps/mobile/src/screens/Home/shared.tsx
+++ apps/mobile/src/screens/Home/shared.tsx
@@ -1,9 +1,9 @@
 import type { ImageSourcePropType } from 'react-native';
 import { StyleSheet } from 'react-native';
 import Animated, { FadeInDown } from 'react-native-reanimated';
-import { InviteCode } from '@aigo/components/InviteCode';
 import { config } from '@aigo/config';
 import { Align, showModal } from 'empty-modal';
+import { InviteCode } from 'modals/InviteCode';
 import PointPopup from 'modals/PointPopup';
 import mustache from 'mustache';
 import { appState } from 'state/app';
