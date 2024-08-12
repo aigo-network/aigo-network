f5e62096186d386bc1f88b73d42180e68e5a4afc
FME849 <1414439@gmail.com>
2024-08-12 09:06:34 +0700

    feat: impl Reward Tab component

---
+++ apps/mobile/src/screens/Reward/RewardTab.tsx
@@ -0,0 +1,83 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Line, Svg } from 'react-native-svg';
+import CoinStack from '@aigo/components/icon/CoinStack';
+import Ticket from '@aigo/components/icon/Ticket';
+import { appState } from 'state/app';
+import { defaultTheme } from 'utils/global';
+import { useSnapshot } from 'valtio';
+
+const RewardTab = () => {
+	const { content } = useSnapshot(appState);
+	const { balance, myReward, redeemed } = content.screens.reward.rewardTab;
+
+	return (
+		<View style={styles.container}>
+			<TouchableOpacity style={styles.tabWrapper}>
+				<View style={styles.tab}>
+					<CoinStack />
+					<View style={styles.tabInfo}>
+						<Text style={styles.tabTitle}>1400 GO</Text>
+						<Text style={styles.tabSubText}>{balance}</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+			<Svg width="2" height="44" viewBox="0 0 2 44" fill="none">
+				<Line x1="1" x2="1" y2="44" stroke="#D8DADC" strokeDasharray="2 2" />
+			</Svg>
+			<TouchableOpacity style={styles.tabWrapper}>
+				<View style={styles.tab}>
+					<Ticket width={22} />
+					<View style={styles.tabInfo}>
+						<Text style={styles.tabTitle}>{myReward}</Text>
+						<Text style={styles.tabSubText}>{redeemed}</Text>
+					</View>
+				</View>
+			</TouchableOpacity>
+		</View>
+	);
+};
+
+export default RewardTab;
+
+const styles = StyleSheet.create({
+	container: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		paddingVertical: 20,
+		borderRadius: 20,
+		backgroundColor: defaultTheme.bgLight,
+		shadowColor: defaultTheme.textDark60,
+		shadowOpacity: 0.15,
+		shadowRadius: 5,
+		shadowOffset: {
+			width: 0,
+			height: 1,
+		},
+		elevation: 6,
+	},
+	tabWrapper: {
+		flex: 1,
+	},
+	tab: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		gap: 8,
+		flex: 1,
+		paddingHorizontal: 12,
+	},
+	tabInfo: {
+		gap: 4,
+	},
+	tabTitle: {
+		fontSize: 16,
+		lineHeight: 24,
+		fontWeight: '700',
+		color: defaultTheme.textDark90,
+	},
+	tabSubText: {
+		fontSize: 13,
+		lineHeight: 15,
+		fontWeight: '500',
+		color: defaultTheme.textDark70,
+	},
+});
