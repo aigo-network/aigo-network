fe50482b34e4b8f52ae2dfdb1d5fe9ebc693de96
FME849 <1414439@gmail.com>
2024-08-14 10:28:21 +0700

    chore: disable font scaling

--- apps/mobile/src/utils/global.ts
+++ apps/mobile/src/utils/global.ts
@@ -34,10 +34,12 @@ setDefaultProps(Text, {
 		fontFamily: 'Lato',
 		color: defaultTheme.textLight,
 	},
+	allowFontScaling: false,
 });
 setDefaultProps(TextInput, {
 	style: {
 		fontFamily: 'Lato',
 		color: defaultTheme.textDark90,
 	},
+	allowFontScaling: false,
 });
