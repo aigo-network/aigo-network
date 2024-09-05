931ff2fbd198668081f09c51108b6845cb88f948
Tan Le <tanlethanh8266@gmail.com>
2024-09-05 10:58:57 +0700

    chore: decrease highlight text size on mobile

--- apps/landing/src/app/section/Highlight/index.tsx
+++ apps/landing/src/app/section/Highlight/index.tsx
@@ -19,14 +19,23 @@ export default Highlight;
 
 const Container = styled.div`
 	background-color: #34c3f4;
-	padding: 10px 20px;
+	padding: 8px 20px;
+
+	@media (min-width: 576px) {
+		padding: 10px 20px;
+	}
 `;
 
 const TitleLink = styled.a`
-	font-size: 16px;
-	line-height: 24px;
+	font-size: 14px;
+	line-height: 18px;
 	text-align: center;
 	text-decoration: none;
 	color: #000000;
 	display: block;
+
+	@media (min-width: 576px) {
+		font-size: 16px;
+		line-height: 22px;
+	}
 `;
