a17bbd278a22855fcf8fd7671d1d750f7903f930
Tan Le <tanlethanh8266@gmail.com>
2024-09-12 22:47:58 +0700

    feat: add AES utils, simplify naming

--- packages/crypto/src/aes.test.ts
+++ packages/crypto/src/aes.test.ts
@@ -7,34 +7,34 @@ describe('test AES', () => {
 	const unalignedData = crypto.getRandomValues(new Uint8Array(17));
 
 	it('encrypt/decrypt with hardcode passcode, size-aligned data', async () => {
-		const encryptedData = await aes.encryptByPasscode(alignedData, passcode1);
-		const decryptedData = await aes.decryptByPasscode(encryptedData, passcode1);
+		const encryptedData = await aes.encrypt(alignedData, passcode1);
+		const decryptedData = await aes.decrypt(encryptedData, passcode1);
 		expect(alignedData.toString()).toEqual(decryptedData.toString());
 	});
 
 	it('encrypt/decrypt with hardcode passcode, size-unaligned data', async () => {
-		const encryptedData = await aes.encryptByPasscode(unalignedData, passcode1);
-		const decryptedData = await aes.decryptByPasscode(encryptedData, passcode1);
+		const encryptedData = await aes.encrypt(unalignedData, passcode1);
+		const decryptedData = await aes.decrypt(encryptedData, passcode1);
 		expect(unalignedData.toString()).toEqual(decryptedData.toString());
 	});
 
 	it('encrypt/decrypt with random passcode, size-aligned data', async () => {
-		const encryptedData = await aes.encryptByPasscode(alignedData, passcode2);
-		const decryptedData = await aes.decryptByPasscode(encryptedData, passcode2);
+		const encryptedData = await aes.encrypt(alignedData, passcode2);
+		const decryptedData = await aes.decrypt(encryptedData, passcode2);
 		expect(alignedData.toString()).toEqual(decryptedData.toString());
 	});
 
 	it('encrypt/decrypt with random passcode, size-unaligned data', async () => {
-		const encryptedData = await aes.encryptByPasscode(unalignedData, passcode2);
-		const decryptedData = await aes.decryptByPasscode(encryptedData, passcode2);
+		const encryptedData = await aes.encrypt(unalignedData, passcode2);
+		const decryptedData = await aes.decrypt(encryptedData, passcode2);
 		expect(unalignedData.toString()).toEqual(decryptedData.toString());
 	});
 
 	it('encrypt/decrypt with wrong passcode', async () => {
 		try {
-			const encryptedData = await aes.encryptByPasscode(alignedData, passcode1);
+			const encryptedData = await aes.encrypt(alignedData, passcode1);
 			const passcode3 = crypto.getRandomValues(new Uint8Array(6)).toString();
-			await aes.decryptByPasscode(encryptedData, passcode3);
+			await aes.decrypt(encryptedData, passcode3);
 		} catch (err) {
 			const errMessage = (err as Error).message;
 			expect(errMessage).toEqual(
--- packages/crypto/src/aes.ts
+++ packages/crypto/src/aes.ts
@@ -11,7 +11,7 @@ const AES_BLOCK_SIZE = 16;
 const AES_GCM_IV_SIZE = 12;
 const AES_256_GCM_KEY_LENGTH = 256;
 
-type PasscodeEncrypted = {
+export type Encrypted = {
 	iv: Uint8Array;
 	salt: Uint8Array;
 	encrypted: Uint8Array;
@@ -50,14 +50,11 @@ const deriveKey = async (
 };
 
 /**
- * AES encrypting with passcode
+ * AES encrypting with key string
  * Ref: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/encrypt#aes-gcm_2
  */
-const encryptByPasscode = async (
-	data: Uint8Array,
-	passcode: string,
-): Promise<PasscodeEncrypted> => {
-	const passcodeBytes = new TextEncoder().encode(passcode);
+const encrypt = async (data: Uint8Array, key: string): Promise<Encrypted> => {
+	const passcodeBytes = new TextEncoder().encode(key);
 	const salt = crypto.getRandomValues(new Uint8Array(COMMON_PBKDF2_SALT_SIZE));
 	const derivedKey = await deriveKey(passcodeBytes, salt);
 	const iv = crypto.getRandomValues(new Uint8Array(AES_GCM_IV_SIZE));
@@ -72,23 +69,66 @@ const encryptByPasscode = async (
 };
 
 /**
- * AES decrypting with passcode
+ * AES decrypting with key string
  * Ref: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/decrypt#aes-gcm
  */
-const decryptByPasscode = async (
-	encryptedData: PasscodeEncrypted,
-	passcode: string,
+const decrypt = async (
+	encrypted: Encrypted,
+	key: string,
 ): Promise<Uint8Array> => {
-	const passcodeBytes = new TextEncoder().encode(passcode);
-	const derivedKey = await deriveKey(passcodeBytes, encryptedData.salt);
+	const passcodeBytes = new TextEncoder().encode(key);
+	const derivedKey = await deriveKey(passcodeBytes, encrypted.salt);
 
 	const data = await crypto.subtle.decrypt(
-		{ name: 'AES-GCM', iv: encryptedData.iv },
+		{ name: 'AES-GCM', iv: encrypted.iv },
 		derivedKey,
-		encryptedData.encrypted,
+		encrypted.encrypted,
 	);
 
 	return new Uint8Array(data);
 };
 
-export const aes = { encryptByPasscode, decryptByPasscode };
+const encryptAndMerge = async (
+	data: Uint8Array,
+	passcode: string,
+): Promise<Uint8Array> => {
+	const encrypted = await encrypt(data, passcode);
+
+	return mergeEncrypted(encrypted);
+};
+
+const mergeEncrypted = (encrypted: Encrypted) => {
+	const { iv, salt, encrypted: encryptedData } = encrypted;
+	const merged = new Uint8Array(iv.length + salt.length + encryptedData.length);
+
+	merged.set(iv, 0);
+	merged.set(salt, iv.length);
+	merged.set(encryptedData, iv.length + salt.length);
+
+	return merged;
+};
+
+const separateEncrypted = (merged: Uint8Array): Encrypted => {
+	if (merged.length < AES_GCM_IV_SIZE + COMMON_PBKDF2_SALT_SIZE) {
+		throw new Error(
+			'Merged data is too short to contain the IV, salt, and encrypted data.',
+		);
+	}
+
+	const iv = merged.slice(0, AES_GCM_IV_SIZE);
+	const salt = merged.slice(
+		AES_GCM_IV_SIZE,
+		AES_GCM_IV_SIZE + COMMON_PBKDF2_SALT_SIZE,
+	);
+	const encrypted = merged.slice(AES_GCM_IV_SIZE + COMMON_PBKDF2_SALT_SIZE);
+
+	return { iv, salt, encrypted };
+};
+
+export const aes = {
+	encrypt,
+	decrypt,
+	encryptAndMerge,
+	mergeEncrypted,
+	separateEncrypted,
+};
