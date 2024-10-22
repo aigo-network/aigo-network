26e23ec142ebbf7e0782a85f9c9d15683020d57c
aigo <yuehua5367@outlook.com>
2024-10-22 16:01:11 +0800

    chore(lint): lint files

--- apps/landing/src/app/section/MapEmbedding/index.tsx
+++ apps/landing/src/app/section/MapEmbedding/index.tsx
@@ -1,17 +1,17 @@
 import './index.css';
 
 export default function MapEmbedding() {
-    return (
-        <div className="map-container">
-            <div className="map-embedding">
-                <iframe
-                    className="map-iframe"
-                    src="https://depinscan.io/widget/map/aigo-network"
-                    name="showIframe"
-                    width="100%"
-                    height="600px"
-                ></iframe>
-            </div>
-        </div>
-    );
+	return (
+		<div className="map-container">
+			<div className="map-embedding">
+				<iframe
+					className="map-iframe"
+					src="https://depinscan.io/widget/map/aigo-network"
+					name="showIframe"
+					width="100%"
+					height="600px"
+				></iframe>
+			</div>
+		</div>
+	);
 }
